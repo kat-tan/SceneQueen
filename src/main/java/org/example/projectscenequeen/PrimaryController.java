63d989469434e5006cf4290b12e139f33510cade
kathleentan <tank@farmingdale.edu>
2024-03-30 11:02:52 -0400

    no splashscreen, setRoot working

--- src/main/java/org/example/projectscenequeen/PrimaryController.java
+++ src/main/java/org/example/projectscenequeen/PrimaryController.java
@@ -1,7 +1,42 @@
 package org.example.projectscenequeen;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+import static org.example.projectscenequeen.SceneQueenApp.scene;
+
 public class PrimaryController {
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    protected void handleSubmitButton() throws IOException {
+        SceneQueenApp.setRoot("MainPage");
+        System.out.println("next page");
+    }
+
+    @FXML
+    protected void forgotPasswordPrompt(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    protected void googleLoginAction(MouseEvent mouseEvent) throws IOException {
+        SceneQueenApp.setRoot("MainPage");
+    }
+
+    @FXML
+    protected void signUpHerePrompt(MouseEvent mouseEvent) {
+
+    }
+
     protected void initialize() {
 
     }
+
 }
---
+++ src/main/java/org/example/projectscenequeen/SceneQueenApp.java
@@ -0,0 +1,37 @@
+package org.example.projectscenequeen;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class SceneQueenApp extends Application {
+    public static Scene scene;
+    @Override
+    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(SceneQueenApp.class.getResource("SplashScreen.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+        scene = new Scene(loadFXML("SignInPage"));
+
+        // stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    public static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SceneQueenApp.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

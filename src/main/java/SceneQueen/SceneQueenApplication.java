0d656ee9623f970cf4d112c04ab0a3113feac429
Kathleen Tan <tank@farmingdale.edu>
2024-05-06 02:38:41 -0400

    Added ToolTips to NewProject, Added ContinueProject page

--- src/main/java/SceneQueen/SceneQueenApplication.java
+++ src/main/java/SceneQueen/SceneQueenApplication.java
@@ -1,4 +1,4 @@
-package org.CSC325Capstone.projectscenequeen;
+package SceneQueen;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -16,14 +16,13 @@ import com.google.firebase.cloud.FirestoreClient;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class SceneQueenApp extends Application {
+public class SceneQueenApplication extends Application {
     public static Scene scene;
     public static Firestore fstore;
     public static FirebaseAuth fauth;
 
     @Override
     public void start(Stage stage) throws IOException {
-
         FileInputStream serviceAccount = new FileInputStream
                 ("src/main/resources/org/example/projectscenequeen/json/scenequeen-firebase-adminsdk.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
@@ -34,20 +33,27 @@ public class SceneQueenApp extends Application {
         fstore = FirestoreClient.getFirestore();
         fauth = FirebaseAuth.getInstance();
 
-        scene = new Scene(loadFXML("Welcome"));
+        scene = new Scene(loadFXML("SignIn"));
         stage.setScene(scene);
+        stage.sizeToScene();
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
         scene.getWindow().sizeToScene();
         scene.getWindow().centerOnScreen();
     }
 
     public static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SceneQueenApp.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SceneQueenApplication.class.getResource(
+                "/org/example/projectscenequeen/FXML/" + fxml + ".fxml")
+        );
         return fxmlLoader.load();
+    }
+
+    public static Firestore getFirestore() {
+        return FirestoreClient.getFirestore();
     }
 
     public static void main(String[] args) {

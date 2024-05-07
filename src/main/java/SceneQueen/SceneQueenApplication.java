4a815982e56b6c447a87605e406c12f5ca732535
Kathleen Tan <tank@farmingdale.edu>
2024-05-07 01:23:52 -0400

    Meet the Team page updated

--- src/main/java/SceneQueen/SceneQueenApplication.java
+++ src/main/java/SceneQueen/SceneQueenApplication.java
@@ -39,7 +39,6 @@ public class SceneQueenApplication extends Application {
         scene = new Scene(loadFXML("SignIn"));
         stage.setScene(scene);
         stage.sizeToScene();
-        stage.setResizable(false);
         stage.show();
     }
 
